2604f07f124e86919054463b29c990df3f4757c0
Scott McKeon <scottmckeon@gmail.com>
2018-04-29 07:36:33 -0700

    Update weather app to use axios

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const yargs = require('yargs');
-
-const geocode = require('./geocode/geocode');
-const weather = require('./weather/weather');
+const axios = require('axios');
+require('dotenv').config();
 
 const argv = yargs
   .options({
@@ -16,19 +15,28 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-  if(errorMessage) {
-    console.log(errorMessage);
+const encodedAddress = encodeURIComponent(argv.address);
+const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${process.env.GOOGLE_MAP_API_KEY}`;
+
+axios.get(geocodeUrl).then((response) => {
+  if (response.data.status === 'ZERO_RESULTS' || response.data.status === 'INVALID_REQUEST') {
+    throw new Error('Unable to find that address');
+  }
+
+  let latitude = response.data.results[0].geometry.location.lat;
+  let longitude = response.data.results[0].geometry.location.lng;
+  console.log('address: ', response.data.results[0].formatted_address);
+  const weatherUrl = `https://api.darksky.net/forecast/${process.env.DARK_SKIES_API_KEY}/${latitude},${longitude}`;
+
+  return axios.get(weatherUrl);
+}).then((res) => {
+  let temperature = res.data.currently.temperature;
+  let apparentTemperature = res.data.currently.apparentTemperature;
+  console.log(`It's currently ${temperature}. It feels like ${apparentTemperature}.`);
+}).catch((e) => {
+  if (e.code === 'ENOTFOUND') {
+    console.log('Unable to connect to server');
   } else {
-    console.log(results.address);
-    weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
-      if(errorMessage) {
-        console.log(errorMessage);
-      } else {
-        console.log(`It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemperature}.`);
-      }
-    });
+    console.log(e.message)
   }
 });
-
-
